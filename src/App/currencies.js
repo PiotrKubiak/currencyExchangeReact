f5570005f8adddc9686b7cec24b30188a4f251ac
Piotr Kubiak <kuberxxx1986@gmail.com>
2021-07-09 18:41:02 +0200

    added currency in currencies.js

---
+++ src/App/currencies.js
@@ -0,0 +1,32 @@
+export const currencies = [
+  {
+    short: "EUR",
+    name: "EURO",
+    rate: "4.56",
+  },
+  {
+    short: "GBP",
+    name: "FUNT BRYTYJSKI",
+    rate: "5.29",
+  },
+  {
+    short: "USD",
+    name: "DOLAR USA",
+    rate: "3.80",
+  },
+  {
+    short: "NOK",
+    name: "KORONA NORWESKA",
+    rate: "0.46",
+  },
+  {
+    short: "SEK",
+    name: "KORONA SZWEDZKA",
+    rate: "0.45",
+  },
+  {
+    short: "CHF",
+    name: "FRANK SZWAJCARSKI",
+    rate: "4.13",
+  },
+];
