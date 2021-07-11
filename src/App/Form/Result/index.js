8bc41d9e97e10709ff5fada2c62f5f13b259ce09
Piotr Kubiak <kuberxxx1986@gmail.com>
2021-07-11 15:01:02 +0200

    add index.js in Result

---
+++ src/App/Form/Result/index.js
@@ -0,0 +1,14 @@
+import "./style.css";
+
+export const Result = ({ result }) => (
+  <p className="result">
+    {result !== undefined && (
+      <>
+        {result.sourceAmount.toFixed(2)}&nbsp;PLN&nbsp;=&nbsp;
+        <strong>
+          {result.targetAmount.toFixed(2)}&nbsp;{result.currency}
+        </strong>
+      </>
+    )}
+  </p>
+);
