54fad5fe8abe806bb062aefa3f452249459c1bdc
Piotr Kubiak <kuberxxx1986@gmail.com>
2021-08-13 11:41:10 +0200

    delete comments

--- src/App/Clock/index.js
+++ src/App/Clock/index.js
@@ -10,7 +10,6 @@ const formatDate = (date) =>
     day: "numeric",
     month: "long",
   });
-//Funkcja, konwertuje datę i godzinę na łańcuch znaków wg czasu z komputera
 export const Clock = () => {
   const [date, setDate] = useState(new Date());
 
@@ -18,12 +17,9 @@ export const Clock = () => {
     const intervalID = setInterval(() => {
       setDate(new Date());
     }, 1000);
-    //Ustawiamy tak aby nasz zegar przestawiał się co sekundę
     return () => {
       clearInterval(intervalID);
     };
   }, []);
-  //Czyścimy interwał
   return <div className="clock">Dzisiaj jest {formatDate(date)}</div>;
-  //Zwracamy naszą datę
 };
